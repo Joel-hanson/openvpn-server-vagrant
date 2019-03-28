696be611bb14d42b095930d20958db178776cb6a
Joel-hanson <joelhanson025@gmail.com>
2019-03-28 16:02:53 +0530

    hold added

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,5 +4,7 @@
 sudo apt-get -y update
 
 # Update Ubuntu
+sudo apt-mark hold grub-pc
+sudo apt-mark hold grub-pc-bin
 sudo apt-get -y upgrade || sudo dpkg --configure -a
 sudo apt-get -y dist-upgrade
